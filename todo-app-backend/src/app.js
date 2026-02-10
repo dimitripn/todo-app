566b85f50adb3ee753ac7e9472918e4419263ce2
dimitripn <dimitriprimanugraha@gmail.com>
2026-02-10 15:38:43 +0700

    Add CORS support and fix import typo in app.js

--- todo-app-backend/src/app.js
+++ todo-app-backend/src/app.js
@@ -1,34 +1,43 @@
-import exress from "express";
+import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import taskRoutes from "./routes/task.route.js";
 import sequelize from "./config/sequelize.js";
 
-const app = exress();
+const app = express();
 
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Database connected...");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+	.authenticate()
+	.then(() => {
+		console.log("Database connected...");
+	})
+	.catch((err) => {
+		console.error("Unable to connect to the database:", err);
+	});
 
 sequelize
-  .sync()
-  .then(() => {
-    console.log("Database synchronized...");
-  })
-  .catch((err) => {
-    console.error("Error synchronizing the database:", err);
-  });
+	.sync()
+	.then(() => {
+		console.log("Database synchronized...");
+	})
+	.catch((err) => {
+		console.error("Error synchronizing the database:", err);
+	});
 
-app.use(exress.json());
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		allowedHeaders: ["Content-Type", "Authorization"],
+	}),
+);
+
+app.use(express.json());
+
 app.use("/auth", authRoutes);
 app.use("/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Todo App API");
+	res.send("Welcome to the Todo App API");
 });
 
 export default app;
